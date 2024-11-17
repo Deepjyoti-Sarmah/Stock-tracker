5978d3d5502c6e278d6b2a3fcab7227b6be193f8
Deepjyoti Sarmah <deepjyotisarmah37@gmail.com>
2024-11-17 14:19:01 +0530

    fixing network error in react native

--- mobile/network.ts
+++ mobile/network.ts
@@ -1,9 +1,9 @@
 import { Platform } from "react-native";
 
 export const baseUrl = (scheme: "http" | "ws") => {
-    const PORT = 3000;
-    const HOST = Platform.OS === "android" ? "10.0.2.2" : "localhost";
+  const PORT = 3000;
+  const HOST = Platform.OS === "android" ? "192.168.165.165" : "localhost";
+  //    const HOST = Platform.OS === "android" ? "172.19.0.1 " "10.0.2.2" : "localhost";
 
-    return `${scheme}://${HOST}:${PORT}`;
-
+  return `${scheme}://${HOST}:${PORT}`;
 }
